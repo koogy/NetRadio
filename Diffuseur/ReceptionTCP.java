9bfc6afd854e7da031b13fd8590bb6089ebde6af
Alex <lyalex98@informatique.univ-paris-diderot.fr>
2021-04-27 22:16:27 +0200

    wrong port, message null check

--- Diffuseur/ReceptionTCP.java
+++ Diffuseur/ReceptionTCP.java
@@ -12,19 +12,42 @@ public class ReceptionTCP implements Runnable {
         this.diffuseur = diffuseur;
     }
 
+    public String formatMessage(String message) {
+        return message + "\r\n";
+    }
+
+    public void sendMessage(PrintWriter pw, String message) {
+        pw.print(formatMessage(message));
+        pw.flush();
+    }
+
     @Override
     public void run() {
         try {
-            ServerSocket server = new ServerSocket(diffuseur.multidiffusion_port);
+            ServerSocket server = new ServerSocket(diffuseur.tcp_port);
             while (true) {
                 Socket socket = server.accept();
                 BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 
                 String message = br.readLine();
+
+                /*
+                 * Un diffuseur peut de plus recevoir des messages d’utilisateurs qu’il pourra
+                 * diffuser
+                 */
+                if (message != null) {
+                    if (message.startsWith("MESS ")) {
+                        diffuseur.diffuseur_messages.add(message.substring(5, message.length()));
+                        sendMessage(pw,message);
+                    }
+                  
+                }
+               
                 System.out.println("Message recu :" + message);
-                pw.println(message);
-                pw.flush();
+                sendMessage(pw,message);
+
+
                 br.close();
                 pw.close();
                 socket.close();
